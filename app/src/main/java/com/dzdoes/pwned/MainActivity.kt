4178ca9643beb756c9514d689eb1018ccdaf392d
merwanezio <merwanezio@gmail.com>
2019-04-03 16:04:53 +0100

    1

--- app/src/main/java/com/dzdoes/pwned/MainActivity.kt
+++ app/src/main/java/com/dzdoes/pwned/MainActivity.kt
@@ -16,13 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_send.setOnSwipeListener {
+            search()
+        }
+
+
+    }
+
+    private fun search() {
+
+        if(!validEmail(et_email.text))
+            return
+
         var gson = GsonBuilder()
             .setLenient()
             .create()
 
-
         val retrofit = Retrofit.Builder()
-
             .baseUrl("https://haveibeenpwned.com/api/")
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
@@ -30,21 +40,23 @@ class MainActivity : AppCompatActivity() {
         val pwned = retrofit.create(PwnedService::class.java)
 
 
-
-        btn_send.setOnClickListener(View.OnClickListener {
-            var email = et_email.text.toString()
-            AppExecutors.instance.networkIO().execute {
-               val e = pwned.getAllBraches(email).execute()
-                Log.e("result",e.raw().toString())
-
-                runOnUiThread {
-                    showResult(e.body())
-                }
-
+        var email = et_email.text.toString()
+        AppExecutors.instance.networkIO().execute {
+            val e = pwned.getAllBraches(email).execute()
+            Log.e("result",e.raw().toString())
+            runOnUiThread {
+                showResult(e.body())
             }
-        })
+        }
+
+        btn_send.showResultIcon(true)
 
     }
+
+    private fun validEmail(email:String): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
     private fun showResult(body: List<Breach>?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
